eecddbae7d6f7ff7998b1ab3745de61c04456836
Murat Ozcan <muratkerem@gmail.com>
2022-12-29 09:04:58 -0600

    refined plugin and task files

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,6 +1,7 @@
 describe('e2e sanity', () => {
   it('passes sanity', () => {
     cy.visit('/')
-    cy.getByCy('count').contains('count is 0').dblclick().contains('count is 2')
+    cy.getByCy('greeting').should('be.visible')
+    cy.task('log', 'e2e sanity passed')
   })
 })

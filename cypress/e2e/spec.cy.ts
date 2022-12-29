64bc3ced546497716106dcec03f32835ca2d8c40
Murat Ozcan <muratkerem@gmail.com>
2022-12-29 09:14:02 -0600

    corrected the spec

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,7 +1,7 @@
 describe('e2e sanity', () => {
   it('passes sanity', () => {
     cy.visit('/')
-    cy.getByCy('greeting').should('be.visible')
+    cy.getByCy('count').contains('count is 0').dblclick().contains('count is 2')
     cy.task('log', 'e2e sanity passed')
   })
 })
